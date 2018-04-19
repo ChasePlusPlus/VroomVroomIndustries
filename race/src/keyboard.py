8132f071e7eb7e8afb46ee257304522a8c27076e
ChasePlusPlus <chd5hq@virginia.edu>
2018-04-19 19:35:53 -0400

    keyboard done

--- race/src/keyboard.py
+++ race/src/keyboard.py
@@ -3,8 +3,18 @@
 import rospy
 from race.msg import drive_param # import the custom message
 import curses
+
 forward = 0;
-left = 0;
+turn = 0;
+increment = 0.1
+
+n = raw_input("Change increment? ")
+if n == "":
+   print "default set to 0.1", "\r"
+else:
+   increment = int(n)
+   print "increment set to: ", increment, "\r"
+
 
 stdscr = curses.initscr()
 curses.cbreak()
@@ -12,37 +22,67 @@ stdscr.keypad(1)
 rospy.init_node('keyboard_talker', anonymous=True)
 pub = rospy.Publisher('drive_parameters', drive_param, queue_size=10)
 
+
+
 stdscr.refresh()
 
 key = ''
+forward = 0 #positive is forward, negative is backwards
+turn = 0 #positive is right, negative is left
+
+
+
+	
 while key != ord('q'):
 	key = stdscr.getch()
 	stdscr.refresh()
         
 
         # fill in the conditions to increment/decrement throttle/steer
-
+	
+	
 	if key == curses.KEY_UP:
-            forward = 0 
-	    print("key up ^\n")       
+	    if forward <= 100:
+		forward += increment
+		print "Throttle up: ", forward, "\r" 
+	    else:
+		print "MAX THROTTLE", "\r"
+    
 	elif key == curses.KEY_DOWN:
-            forward = 0 
-	    print("key down \/\n")
+	    if forward >= -100:
+                forward -= increment
+	        print "Throttle down: ", forward, "\r"
+	    else:
+	        print "MAX REVERSE", "\r"
+
 	if key == curses.KEY_LEFT:
-            left = 0
-	    print("left <-\n")
+	    if turn >= -100:
+            	turn -= increment
+	    	print "LEFT: ", turn, "\r"
+	    else:
+		print "MAX LEFT", "\r"
+
 	elif key == curses.KEY_RIGHT:
-            left = 0
-	    print("right ->\n")
+            if turn <= 100:
+            	turn += increment
+	    	print "RIGHT: ", turn, "\r"
+	    else:
+		print "MAX RIGHT", "\r"
+
         elif key == curses.KEY_DC:
             # this key will center the steer and throttle
             forward = 0
-	elif key == 113:
-	    print("quit")
-	    break
+	    turn = 0
+	    print "THROTTLE/STEER RESET!", "\r"
+
+	elif key == ord('r'):   #reset key for mac users
+	    forward = 0
+            turn = 0
+	    print "THROTTLE/STEER RESET!", "\r"
+
 	msg = drive_param()
 	msg.velocity = forward
-	msg.angle = left
+	msg.angle = turn
 	pub.publish(msg)
 
 curses.endwin()
